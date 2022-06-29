997172a2ae753fe6a755ba2d72c1d7cfab807ce6
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-06-29 12:27:23 +0200

    add Header component.

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Navbar, Container, Nav, Form, Dropdown, Badge } from "react-bootstrap";
+import { FaShoppingCart } from "react-icons/fa";
+
+function Header() {
+  return (
+    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+      <Container className="d-flex ">
+        <Navbar.Brand>
+          <Nav.Link href="#features">Shopping card</Nav.Link>
+        </Navbar.Brand>
+
+        <Navbar.Text className="search flex-fill" style={{ maxWidth: 700 }}>
+          <Form.Control
+            placeholder="Search a product"
+            className="m-auto w-100 d-inline-block"
+            aria-label="Search"
+          />
+        </Navbar.Text>
+
+        <Nav>
+          <Dropdown alignLeft className="m-3 " align= "end" >
+            <Dropdown.Toggle variant="success">
+              <FaShoppingCart color="white" fontSize="25px" />
+              <Badge className="bg-transparent">13</Badge>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu style={{ minWidth: 370 }} className="mt-2">
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
