08a372fbac2129489eed2ff178f353711a040728
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-06-29 14:22:38 +0200

    create file of signleProduct

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { CartState } from "../context/Context";
+import SingleProduct from "./SingleProduct";
 
 function Home() {
   const {
     state: { products },
   } = CartState();
 
-
   console.log(products);
-  return <div className='home'>
-    {/* <Filters/> */}
-    <div className='productContainer'>
-        
+  return (
+    <div className="home">
+      {/* <Filters/> */}
+      <div className="productContainer">
+        {products.map((prod) => (
+          <SingleProduct prod={prod} key={prod.id} />
+        ))}
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default Home;
---
+++ src/components/SingleProduct.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SingleProduct({ prod }) {
+  return <div>{prod.name}</div>;
+}
+
+export default SingleProduct;
