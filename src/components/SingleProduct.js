2ddaa8a4363dcb09d8c1a17c3cfa23e4fac19f55
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-06-30 12:33:13 +0200

    add buttons add/remove from cart, out of stock.

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,7 +1,59 @@
 import React from "react";
+import { Card, Button } from "react-bootstrap";
+import Rating from "./Rating";
+import { CartState } from "../context/Context";
 
 function SingleProduct({ prod }) {
-  return <div>{prod.name}</div>;
+  const {
+    state: { cart },
+    dispatch,
+  } = CartState();
+  return (
+    <div className="products">
+      <Card>
+        <Card.Img variant="top" src={prod.image} alt={prod.name} />
+        <Card.Body>
+          <Card.Title>{prod.name}</Card.Title>
+          <Card.Subtitle className="pb-3">
+            <span>{prod.price.split(".")[0]} $</span>
+            {prod.fastDelivery ? (
+              <div>Fast delivery</div>
+            ) : (
+              <div> 5 days delivery</div>
+            )}
+          </Card.Subtitle>
+          <Rating rating={prod.ratings} />
+
+          {cart.some((p) => p.id === prod.id) ? (
+            <Button
+              onClick={() =>
+                dispatch({
+                  type: "REMOVE_FROM_CART",
+                  payload: prod,
+                })
+              }
+              variant="danger"
+            >
+              Remove from cart
+            </Button>
+          ) : (
+            <Button
+              onClick={() =>
+                dispatch({
+                  type: "ADD_TO_CART",
+                  payload: prod,
+                })
+              }
+              variant="primary"
+              disabled={prod.inStock}
+            >
+              {prod.inStock ? "Out of stock" : "Add to cart"}
+            </Button>
+          )}
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default SingleProduct;
--- src/context/Context.js
+++ src/context/Context.js
@@ -3,15 +3,14 @@ import { faker } from "@faker-js/faker";
 import { cartReducer } from "./Reducers";
 
 const Cart = createContext();
-faker.seed(99);
+faker.seed(20);
 function Context({ children }) {
-
   const products = [...Array(20)].map(() => {
     return {
       id: faker.datatype.uuid(),
       name: faker.commerce.productName(),
       price: faker.commerce.price(),
-      image: faker.image.abstract(),
+      image: faker.image.abstract(640, 480, true),
       inStock: faker.helpers.arrayElement([0, 3, 5, 6, 7]),
       fastDelivery: faker.datatype.boolean(),
       ratings: faker.helpers.arrayElement([1, 2, 3, 4, 5]),
