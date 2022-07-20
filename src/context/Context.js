6098c2c420509125f9aaa0ee3a77c6edaedba377
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-07-20 22:43:25 +0200

    add gh-pages

--- src/context/Context.js
+++ src/context/Context.js
@@ -3,7 +3,9 @@ import { faker } from "@faker-js/faker";
 import { cartReducer, productReducer } from "./Reducers";
 
 const Cart = createContext();
+
 faker.seed(20);
+
 function Context({ children }) {
   const products = [...Array(20)].map(() => {
     return {
