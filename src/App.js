d71f9d9ea63aa13b21ddf53b1ab5b7901f0020b8
Piotr Matlak <piotr.matlak.contact@gmail.com>
2023-11-11 19:09:48 +0100

    fix dependencies and gh-pages

--- src/App.js
+++ src/App.js
@@ -2,21 +2,18 @@ import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Cart from "./components/Cart";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  useRouteMatch,
-  useParams,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router
+      basename={
+        process.env.NODE_ENV === "development" ? "/" : "/shopping-cart/"
+      }
+    >
       <Header />
       <Routes>
-        <Route path="/"  element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
